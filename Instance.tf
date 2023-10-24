6518fc92771d2445ff02e77719e1aa403cbd0c3e
Kevin Pavon <kevin.pavon.kp@gmail.com>
2023-10-24 00:03:56 -0600

    cambios para la instancia EC2

--- Instance.tf
+++ Instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "angular_instance" {
   instance_type = "t2.micro" 
   key_name = aws_key_pair.kevin_challenge1.key_name
   subnet_id = aws_subnet.subnet_kevin_public.id 
-  vpc_security_group_ids = [aws_security_group.security_group_challenge1]
+  vpc_security_group_ids = [aws_security_group_challenge1.id]
 
   tags = {
     Name = "angular-instance"
