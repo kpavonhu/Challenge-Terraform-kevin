88c856a061775f033ab918cf0f40dba436a89a2f
Kevin Pavon <kevin.pavon.kp@gmail.com>
2023-10-23 23:15:10 -0600

    Cambios

--- Instance.tf
+++ Instance.tf
@@ -2,7 +2,7 @@
 
 resource "aws_instance" "angular_instance" {
   ami           = "ami-053b0d53c279acc90" # OS ubuntu
-  instance_type = "t3.micro" 
+  instance_type = "t2.micro" 
   key_name = aws_key_pair.kevin_challenge1.key_name
   subnet_id = aws_subnet.subnet_kevin_public.id 
   vpc_security_group_ids = [aws_security_group.security_group_challenge1]
